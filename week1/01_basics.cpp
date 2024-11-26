ee12e72bc3694d0ef34b170c8ec90faf83857884
HassanDeveloperXDesigner <hassana18200@gmail.com>
2024-11-26 21:04:36 +0500

    Add files via upload

---
+++ week1/01_basics.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    // C++ basic structure
+    cout<<"#########################"<<endl;
+    cout<<"#                       #"<<endl;
+    cout<<"#                       #"<<endl;
+    cout<<"#                       #"<<endl;
+    cout<<"#########################"<<endl;
+    
+    return 0;
+}
---
+++ week1/02_output.cpp
@@ -0,0 +1,21 @@
+# include <iostream>
+using namespace std;
+
+main(){
+
+    // text and numbers output
+    cout<<"text"<<1234<<endl;
+
+    // output with calculation
+    cout<<1+2<<endl;
+    cout<<1*2<<endl;
+    cout<<1/2<<endl;
+
+    // escape sequences
+    cout<<12345<<"\n\n ";
+    cout<<"this is \"a\" text\n";
+    cout<<"this is a text"<<endl;
+    cout<<"\tthis is a text\n";
+
+    return 0;
+}
