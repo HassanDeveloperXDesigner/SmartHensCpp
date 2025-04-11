4f4045e814ffa4276bf179ed4c6c47cbd1b5e5f1
HassanDeveloper <hassana18200@gmail.com>
2025-04-11 22:34:39 +0500

    Add files via upload

--- week7/signInSignUp.cpp
+++ week7/signInSignUp.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <fstream>
 #include <conio.h>
+
 using namespace std;
 
 /*
@@ -10,17 +12,30 @@ using namespace std;
 5. input validation
 */
 
-string emails[100], passwords[100];
+string usernames[100], emails[100], passwords[100];
+// int prices[100];
 int totalUsers = 0;
 
+
+/*
+Ali     ali@gmail.com     123
+Usman   usman@gmail.com   456
+-----   ---------         ---
+
+*/
+
 void Menu();
-void signUp(string email, string password);
+void signUp(string username , string email, string password);
 bool signIn(string email, string password);
 bool isUserPresent(string email);
-
+string GetField(string data, int feild);
+void LoadData();
+void saveData(string username , string email, string password);
 
 int main()
 {
+    LoadData();
+
     cout << "Welcome to the Sign In/Sign Up page!" << endl;
 
     int choice;
@@ -29,12 +44,14 @@ int main()
         Menu();
         cin >> choice;
 
-        string email, password;
+        string username, email, password;
         switch (choice)
         {
         case 1:
             // Sign Up
             cout << "Sign Up" << endl;
+            cout << "Enter username: ";
+            cin >> username;
             cout << "Enter your email: ";
             cin >> email;
             cout << "Enter your password: ";
@@ -46,7 +63,8 @@ int main()
             }
             else
             {
-                signUp(email, password);
+                signUp(username , email, password);
+                saveData(username , email, password);
             }
             break;
         case 2:
@@ -73,8 +91,9 @@ int main()
     return 0;
 }
 
-void signUp(string email, string password)
+void signUp(string username , string email, string password)
 {
+    usernames[totalUsers] = username;
     emails[totalUsers] = email;
     passwords[totalUsers] = password;
     totalUsers++;
@@ -116,3 +135,49 @@ void Menu()
     cout << "3. Exit" << endl;
     cout << "Enter your choice: ";
 }
+
+void LoadData()
+{
+    ifstream file("users.txt");
+    string data;
+    while(!file.eof())
+    {
+        getline(file,data);
+        usernames[totalUsers] = GetField(data,1);
+        emails[totalUsers] = GetField(data,2);
+        passwords[totalUsers] = GetField(data,3);
+        // prices[totalUsers] = stoi(GetField(data,4));
+        // stoi = string to int   "int stoi(string)"
+        // stof = string to float   "int stof(string)"
+        totalUsers++;
+
+    }
+    file.close();
+}
+
+string GetField(string data, int feild)
+{
+    //  data = Ali,ali@gmail.com,123
+    // feild = 1
+
+    string fieldData = "";
+    int commaCount = 1;
+    for(int i = 0;i < data.length();i++){
+        if(data[i] == ','){
+            commaCount++;
+        }
+        else if(commaCount == feild){
+            fieldData += data[i];
+        }
+    }
+    return fieldData;
+
+}
+
+void saveData(string username , string email, string password)
+{
+    ofstream file ("users.txt", ios::app);
+    file<<username << "," << email << "," << password << endl;
+    file.close();
+}
+
